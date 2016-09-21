4677b5af6d80a99d09fb845c0816317f728fef1e
Gilad Goldberg <ggoldberg@paypal.com>
2016-09-21 18:45:32 +0300

    Line graph

--- src/routes/ByMerchant/components/ByMerchant.js
+++ src/routes/ByMerchant/components/ByMerchant.js
@@ -1,6 +1,9 @@
+import _ from 'lodash'
+import moment from 'moment'
 import React from 'react'
 
 import TxnList from '../../../components/TxnList'
+import { Line as LineGraph } from 'react-chartjs-2'
 
 export default class ByMerchant extends React.Component {
 
@@ -18,6 +21,33 @@ export default class ByMerchant extends React.Component {
   }
 
   render () {
+    const labels = _(this.props.transactions).map(txn => moment(txn.txn_date).format('l')).value()
+    const data = _(this.props.transactions).map(txn => txn.amount / 100).value()
+    let chartData = {
+      labels,
+      datasets: [
+        {
+          data,
+          borderColor: 'rgba(75,192,192,1)',
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          spanGaps: true
+        }
+      ]
+    }
+    let chartOptions = {
+      legend: {
+        display: false
+      },
+      scales: {
+        yAxes: [
+          { type: 'linear',
+          ticks: {
+            min: 0
+          }
+        }]
+      }
+    }
+
     return (<div>
       <div className='form-group'>
         <label htmlFor='select-merchant'>Merchant</label>
@@ -29,7 +59,8 @@ export default class ByMerchant extends React.Component {
           )}
         </select>
 
-        <TxnList transactions={this.props.transactions} />
+        <TxnList transactions={this.props.transactions}
+          chartType={LineGraph} chartData={chartData} chartOptions={chartOptions} />
       </div>
     </div>)
   }
@@ -39,5 +70,5 @@ ByMerchant.propTypes = {
   merchants: React.PropTypes.array,
   onLoad: React.PropTypes.func,
   onMerchantChange: React.PropTypes.func,
-  transactions: React.propTypes.any
+  transactions: React.PropTypes.any
 }
